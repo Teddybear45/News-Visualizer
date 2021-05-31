9f4734ff1d0a6157b9ba2e24334b0d95c7b6e1b2
Ted Guan <tedguan2005@gmail.com>
2021-05-30 23:22:20 -0700

    front sidebar selector, content + map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,57 @@
-import React, {Component, useState, useEffect} from 'react';
-import logo from './logo.svg';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
+import SidebarSelectors from './ContentComponents/SidebarSelectors';
 
-function App () {
-    const [message, setMessage] = useState("");
+function App() {
+  const [message, setMessage] = useState("");
 
-    useEffect(() => {
-        fetch('/api/hello')
-            .then(response => response.text())
-            .then(message => {
-                setMessage(message);
-            });
-    },[])
-    return (
-        <div className="App">
-            <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h1 className="App-title">{message}</h1>
-            </header>
-            <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-        </div>
-    )
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(response => response.text())
+      .then(message => {
+        setMessage(message);
+      });
+  }, [])
+  {/* <h1 className="App-title">{message}</h1> */}
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h4 className="App-header-title">
+          News Visualizer
+        </h4>
+      </header>
+
+      <section className="App-content-section">
+        <section className="App-sidebar-section">
+          <div className="App-sidebar-selectors">
+            <SidebarSelectors/>
+            
+
+          </div>
+          <div className="App-sidebar-content">
+
+
+
+          </div>
+
+
+
+
+        </section>
+
+        <section className="App-main-section">
+          
+          
+        </section>
+      </section>
+      <div className="App-tutorial-popup">
+
+
+      </div>
+    </div>
+  )
 }
 
 export default App;
---
+++ frontend/src/ContentComponents/SidebarSelectors.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+
+class SidebarSelectors extends Component {
+
+
+
+    render() {
+        return (
+            <div className="SidebarSelectors-main-section">
+                
+                <div>Search holder</div>
+                <div>Search holder</div>
+                <div>Search holder</div>
+
+                <h4>Select By News Source </h4>
+                
+
+            </div>
+        )
+    }
+}
+
+export default SidebarSelectors;
