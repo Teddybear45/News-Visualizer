ca4b38c932b2b0579ec4f8bc93b2dd3bfae47115
Ted Guan <tedguan2005@gmail.com>
2021-05-30 18:49:19 -0700

    react + spring init

---
+++ src/test/java/com/tedg/newsvisualizer/NewsVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tedg.newsvisualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsVisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
