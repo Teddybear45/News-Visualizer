d0bdc842254849f2863f7b226d42996dd3fb2eaf
Ted Guan <tedguan2005@gmail.com>
2021-05-31 17:07:03 -0700

    map react leaflet component test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,40 @@
 import React, { Component, useState, useEffect } from 'react';
 import './App.css';
 import SidebarSelectors from './ContentComponents/SidebarSelectors';
+// import MainMapArea from './MainMapArea'
+
+// import L from 'leaflet';
+// import { MapContainer as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
+
+
+
+// class MapArea extends Component{
+
+//   state = {
+//     lat: 51.505,
+//     lng: -0.09,
+//     zoom: 13
+//   }
+//   render() {
+//     const position = [this.state.lat, this.state.lng]
+
+//     return (
+//       <LeafletMap center={position} zoom={this.state.zoom}>
+//         <TileLayer
+//           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap<a/> contributors'
+//           url="https://{s}.title.openstreetmap.org/{z}/{x}/{y}.png"
+//         />
+//         <Marker position={position}>
+//           <Popup>
+//             A CSS3 Popup. <br /> Customizable
+// 					</Popup>
+//         </Marker>
+//       </LeafletMap>
+//     )
+//   }
+// }
+
+
 
 function App() {
   const [message, setMessage] = useState("");
@@ -12,7 +46,7 @@ function App() {
         setMessage(message);
       });
   }, [])
-  {/* <h1 className="App-title">{message}</h1> */}
+  {/* <h1 className="App-title">{message}</h1> */ }
 
 
   return (
@@ -26,8 +60,8 @@ function App() {
       <section className="App-content-section">
         <section className="App-sidebar-section">
           <div className="App-sidebar-selectors">
-            <SidebarSelectors/>
-            
+            <SidebarSelectors />
+
 
           </div>
           <div className="App-sidebar-content">
@@ -42,8 +76,8 @@ function App() {
         </section>
 
         <section className="App-main-section">
-          
-          
+
+
         </section>
       </section>
       <div className="App-tutorial-popup">
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'leaflet/dist/leaflet.css';
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
