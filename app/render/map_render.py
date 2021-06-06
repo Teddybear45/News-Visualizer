ccddb7c24d61c7969539f7884c8d02a990f5437f
Ted Guan <tedguan2005@gmail.com>
2021-06-06 13:54:57 -0700

    start folium

--- app/render/map_render.py
+++ app/render/map_render.py
@@ -2,3 +2,19 @@ import folium
 from folium.plugins import FastMarkerCluster
 
 
+if __name__ == '__main__':
+    highest_location = [45.5236, -122.6750]
+
+    m = folium.Map(location=highest_location, zoom_start=12, tiles="OpenStreetMap")
+    folium.Marker(
+        [45.3288, -121.6625], popup="<i>Mt. Hood Meadows</i>", tooltip="tooltip123"
+    ).add_to(m)
+
+
+    m.save("index.html")
+
+
+
+
+
+
--- app/source/loc_collection.py
+++
@@ -1,5 +0,0 @@
-class LocationCollection():
-    def __init__(self, articles):
-        self.articles = articles
-
-    
