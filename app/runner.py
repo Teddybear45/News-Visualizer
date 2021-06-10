2caea014bce14ab92668d2790cea286406afb805
Ted Guan <tedguan2005@gmail.com>
2021-06-09 18:25:22 -0700

    fixed map skip on None coord location error

--- app/runner.py
+++ app/runner.py
@@ -1,14 +1,18 @@
+import folium
 import geopy
 import stanza
+import time
 from geopy.extra.rate_limiter import RateLimiter
 
 from app.inspect.analyzer import parse_paper, process_paper
 from app.inspect.geolocator import geo_map
+from app.render.map_render import map_plot_cluster_reg, append_heat_map_layer
 from app.source.collector import collect, get_config
 
 import csv
 
 if __name__ == '__main__':
+    start_time = time.time()
     article_list = collect()
     print(article_list)
     stanza.download('en')
@@ -31,6 +35,11 @@ if __name__ == '__main__':
 
     query_map = geo_map(loc_collection, geocode)
 
+    try:
+        query_map.pop(None)
+    except:
+        print("-- All recognized --")
+
     print(query_map)
 
 
@@ -38,3 +47,14 @@ if __name__ == '__main__':
         writer = csv.writer(outfile)
         for key, val in query_map.items():
             writer.writerow([key, val])
+
+    dark_folium_map = folium.Map(location=[59.338315, 18.089960],
+                                 zoom_start=3,
+                                 tiles='CartoDBdark_matter')
+
+    map_plot_cluster_reg(dark_folium_map, query_map)
+    append_heat_map_layer(dark_folium_map, query_map)
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+
