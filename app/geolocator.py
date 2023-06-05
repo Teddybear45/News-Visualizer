3d20629e51639cc83c082b81ab77c21af286013c
Teddy <46308736+Teddybear45@users.noreply.github.com>
2023-06-05 12:11:42 -0700

    Update geolocator.py

--- app/geolocator.py
+++ app/geolocator.py
@@ -1,6 +1,13 @@
 import pandas as pd
 
 
+# Define the custom function to filter results by state
+def filter_by_state(location, state):
+    if location is not None and location.raw['address'].get('state') == state:
+        return location
+    else:
+        return None
+
 # returns map of location names to coordinates. takes location names and articles map
 def geo_map(loc_article_map, geocode_element):
     geo_loc_article_map = {}
@@ -12,6 +19,10 @@ def geo_map(loc_article_map, geocode_element):
 
     df['point'] = df['location'].apply(
         lambda location: str(location.latitude) + "," + str(location.longitude) if location else None)
+
+    # Filter the results based on a specific state
+    desired_state = 'Washington'  # Replace with the state you want to fence to
+    df['location'] = df['location'].apply(filter_by_state, state=desired_state)
 
     for name, loc in zip(df.get('name_token'), df.get('point')):
         geo_loc_article_map[loc] = (name, loc_article_map[name])
